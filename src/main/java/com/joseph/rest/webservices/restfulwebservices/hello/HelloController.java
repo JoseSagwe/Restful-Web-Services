c4aacd9a9e55eabd82ea67b93377d37b168f6829
JoseSagwe <Josephsagwe421@gmail.com>
2023-08-07 19:30:47 +0300

    First Commit

---
+++ src/main/java/com/joseph/rest/webservices/restfulwebservices/hello/HelloController.java
@@ -0,0 +1,26 @@
+package com.joseph.rest.webservices.restfulwebservices.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello Developer Joseph";
+    }
+
+    @GetMapping("/hello-bean")
+    public HelloBean bean(){
+        return new HelloBean("Hello Developer") ;
+    }
+
+    @GetMapping("/hello-bean/{name}")
+    public HelloBean beanPathVariable(@PathVariable String name){
+        return new HelloBean("Hello Developer " + name) ;
+//        USING STRING FORMAT
+//        return new HelloBean(String.format("Hello Developer,   %s" , name)) ;
+    }
+}
